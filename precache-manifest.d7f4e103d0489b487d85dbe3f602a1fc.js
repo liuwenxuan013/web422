82f98d3637c4240bd7d34f2add4a1213da6b21cc
Wenxuan Liu <wliu122@myseneca.ca>
2019-06-11 08:52:58 -0400

    Assignment 3 release!

---
+++ precache-manifest.d7f4e103d0489b487d85dbe3f602a1fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab6ba39bf0826049803e7d4910e4cc29",
+    "url": "/web422/index.html"
+  },
+  {
+    "revision": "667501258a25d262e96a",
+    "url": "/web422/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "e484775c240c53d8128c",
+    "url": "/web422/static/js/2.a2da8fd0.chunk.js"
+  },
+  {
+    "revision": "667501258a25d262e96a",
+    "url": "/web422/static/js/main.e0fe4dd0.chunk.js"
+  },
+  {
+    "revision": "e2a105bc911f49141e9a",
+    "url": "/web422/static/js/runtime~main.74ce4178.js"
+  }
+]);
